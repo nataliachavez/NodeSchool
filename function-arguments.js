d87e9211d07760ae8dc8625b0b1723cdf462602a
natalia chavez <nataliach995@gmail.com>
2018-11-03 15:55:06 -0500

    mi primer commit :D

---
+++ function-arguments.js
@@ -0,0 +1,10 @@
+function math (x, y, z){
+    console.log(x + (y * z))
+}
+math(53, 61, 67);
+
+    function math2(x, y, z) {
+        return x + (y * z)
+    }
+
+    console.log(math2(53, 61, 67));
---
+++ looping-through-arrays.js
@@ -0,0 +1,5 @@
+var pets = ['cat', 'dog', 'rat', 'cow', 'unicorn'];
+for (var i = 0; i < pets.length; i++){
+    pets[i] = pets[i] + 's';
+}
+console.log(pets)
